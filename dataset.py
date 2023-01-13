1fb21021fb46c5b4d226971f770d9d5b5871050a
jose toledo <104653070+toledojm@users.noreply.github.com>
2023-01-13 16:41:07 +0000

    jose

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ from roboflow import Roboflow
 def importar():
 
     rf = Roboflow(api_key="KOrKgmkGRHqzxP6SuKTp") #se crea la conexion
-    project = rf.workspace("prueba2-8v055").project("waste-jrhiu") # se busca el dataset del proyecto xx de espacio de trabajo ww
-    dataset = project.version(2).download("yolov8",location='datasets') # se descarga a carpeta local
 
+    project = rf.workspace("prueba-catzq").project("prueba-33")
+
+    dataset = project.version(3).download("yolov8",location='datasets')
